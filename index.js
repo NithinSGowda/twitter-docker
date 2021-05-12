04207cfbfa68b804eb5e7d13f2fb78eed044ccaa
Nithin S <nithin.s491@gmail.com>
2021-05-12 19:45:57 +0530

    Added All APIs

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-// Open a realtime stream of Tweets, filtered according to rules
-// https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/quick-start
-// const uri = "mongodb://localhost:27017/smmpanel";
 const uri = "mongodb+srv://stockapp:88888888@cluster0.o8iuu.mongodb.net/smmpanel?authSource=admin&replicaSet=atlas-yebdcz-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
 const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const port = 8080
+var multer = require('multer');
+var upload = multer();
+app.use(express.urlencoded({ extended: true })); 
+app.use(upload.array()); 
+app.use(express.json()); 
+var cors = require('cors')
+app.use(cors())
+var request = require('request');
+const psswd = "pass"
+var stream;
+var streamStatus = false
+
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -20,13 +32,7 @@ const token = 'AAAAAAAAAAAAAAAAAAAAAN6hOAEAAAAA4PVHTg%2Fqr2d2ll1wqS2Z5q%2Bwo%2Fs
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
 
-const rules = [{
-        'value': 'tesla',
-        'tag': 'teslas'
-    }
-];
-
-async function getAllRules() {
+async function getAllRules(res) {
 
     const response = await needle('get', rulesURL, {
         headers: {
@@ -39,131 +45,148 @@ async function getAllRules() {
         throw new Error(response.body);
     }
 
-    return (response.body);
+    res.json(JSON.parse(JSON.stringify(response.body)));
+    res.end()
 }
 
-async function deleteAllRules(rules) {
-
-    if (!Array.isArray(rules.data)) {
-        return null;
-    }
-
-    const ids = rules.data.map(rule => rule.id);
-
-    const data = {
+function deleteRule(term,res) {
+    var options = {
+    'method': 'POST',
+    'url': 'https://api.twitter.com/2/tweets/search/stream/rules',
+    'headers': {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
         "delete": {
-            "ids": ids
-        }
-    }
-
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            "content-type": "application/json",
-            "authorization": `Bearer ${token}`
+            "values": [
+                term
+            ]
         }
     })
 
-    if (response.statusCode !== 200) {
-        throw new Error(response.body);
-    }
-
-    return (response.body);
-
-}
-
-async function setRules() {
-
-    const data = {
-        "add": rules
-    }
-
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            "content-type": "application/json",
-            "authorization": `Bearer ${token}`
-        }
-    })
-
-    if (response.statusCode !== 201) {
-        throw new Error(response.body);
-    }
-
-    return (response.body);
-
-}
-
-function streamConnect(retryAttempt) {
-
-    const stream = needle.get(streamURL, {
-        headers: {
-            "User-Agent": "v2FilterStreamJS",
-            "Authorization": `Bearer ${token}`
-        },
-        timeout: 20000
+    };
+    request(options, function (error, response) {
+        if (error){
+            res.json(error)
+          }else{
+            res.json(JSON.parse(response.body))
+            res.end()
+          }
     });
+}
 
-    stream.on('data', data => {
-        try {
-            const json = JSON.parse(data);
-            // console.log(json);
-            obj={}
-            obj["tweetid"]=json["data"]["id"]
-            obj["data"]=json["data"]["text"]
-            obj["tag"]=json["matching_rules"][0]["tag"]
-            console.log(obj);
-            tweeter.create(obj, function (err, obj2) {
-                if (err)
-                    console.log(err);
-                console.log(obj2)
+function addRule(term,name,res) {
+    var request = require('request');
+    var options = {
+      'method': 'POST',
+      'url': 'https://api.twitter.com/2/tweets/search/stream/rules',
+      'headers': {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "add": [
+          {
+            "value": term,
+            "tag": name
+          }
+        ]
+      })
+    
+    };
+    request(options, function (error, response) {
+      if (error){
+        res.json(error)
+      }else{
+        res.json(JSON.parse(response.body))
+        res.end()
+      }
+    });
+    
+}
+
+app.post('/add', (req, res) => {
+    data = JSON.parse(JSON.stringify(req.body))
+    addRule(data.term,data.name,res)
+})
+
+app.post('/delete', (req, res) => {
+    data = JSON.parse(JSON.stringify(req.body))
+    deleteRule(data.term,res)
+})
+
+app.get('/getAll', (req, res) => {
+    data = JSON.parse(JSON.stringify(req.body))
+    getAllRules(res)
+})
+
+app.get('/start/:psswd', (req, res) => {
+    if(req.params.psswd == psswd){
+        if(streamStatus){
+            res.end("Stream already Started")
+        }else{
+            stream = needle.get(streamURL, {
+                headers: {
+                    "User-Agent": "v2FilterStreamJS",
+                    "Authorization": `Bearer ${token}`
+                },
+                timeout: 20000
             });
-            // A successful connection resets retry count.
-            retryAttempt = 1;
-        } catch (e) {
-            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
-                console.log(data.detail)
-                process.exit(1)
-            } else {
-                // Keep alive signal received. Do nothing.
-            }
+            stream.on('data', data => {
+                try {
+                    const json = JSON.parse(data);
+                    // console.log(json);
+                    obj={}
+                    obj["tweetid"]=json["data"]["id"]
+                    obj["data"]=json["data"]["text"]
+                    obj["tag"]=json["matching_rules"][0]["tag"]
+                    console.log(obj);
+                    tweeter.create(obj, function (err, obj2) {
+                        if (err)
+                            console.log(err);
+                        console.log(obj2)
+                    });
+                    // A successful connection resets retry count.
+                    retryAttempt = 1;
+                } catch (e) {
+                    if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+                        console.log(data.detail)
+                        process.exit(1)
+                    } else {
+                        // Keep alive signal received. Do nothing.
+                    }
+                }
+            }).on('err', error => {
+                if (error.code !== 'ECONNRESET') {
+                    console.log(error.code);
+                    process.exit(1);
+                } else {
+                    console.log("AlertAdmin");
+                }
+            });
+            streamStatus=true
+            res.end("Stream started successfully")
         }
-    }).on('err', error => {
-        if (error.code !== 'ECONNRESET') {
-            console.log(error.code);
-            process.exit(1);
-        } else {
-            // This reconnection logic will attempt to reconnect when a disconnection is detected.
-            // To avoid rate limits, this logic implements exponential backoff, so the wait time
-            // will increase if the client cannot reconnect to the stream. 
-            setTimeout(() => {
-                console.warn("A connection error occurred. Reconnecting...")
-                streamConnect(++retryAttempt);
-            }, 2 ** retryAttempt)
-        }
-    });
-
-    return stream;
-
-}
-
-
-(async () => {
-    let currentRules;
-
-    try {
-        // Gets the complete list of rules currently applied to the stream
-        currentRules = await getAllRules();
-
-        // Delete all rules. Comment the line below if you want to keep your existing rules.
-        await deleteAllRules(currentRules);
-
-        // Add rules to the stream. Comment the line below if you don't want to add new rules.
-        await setRules();
-
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
+    }else{
+        res.end("Unauthorised")
     }
+})
+app.get('/stop/:psswd', (req, res) => {
+    if(req.params.psswd == psswd){
+        if(streamStatus){
+            stream.request.abort()
+            streamStatus=false
+            res.end("Stream stopped successfully")
+        }else{
+            res.end("Stream already Stopped")
+        }
+    }else{
+        res.end("Unauthorised")
+    }
+})
 
-    // Listen to the stream.
-    streamConnect(0);
-})();
+
+
+
+app.listen(process.env.PORT||port, () => console.log(`Example app listening on port port!`))
